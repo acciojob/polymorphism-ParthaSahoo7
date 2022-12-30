bb958e40055c14317be526e13b4d469fdfdac216
Partha sarathi <parthasahoo2677@gmail.com>
2022-12-30 11:57:04 +0530

    first commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,27 @@
 package com.driver;
 
+import java.sql.SQLOutput;
+
 public class Main {
+    public static void main(String args[]){
+        Product p = new Product();
+        int ans = p.product(2,4);
+        int ans1 = p.product(3,4,5);
+        double ans2 = p.product(1.5,2.2);
+        System.out.println(ans + " " + ans1 + " " + ans2);
+
+    }
+    public static class Product{
+        public int product(int x , int y){
+            return x*y;
+        }
+        public int product(int x, int y, int z){
+            return x*y*z;
+        }
+        public double product(double x , double y){
+            return x*y;
+        }
+
+    }
 
 }
